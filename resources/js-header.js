34c24fb8e8067c292450eb9938e85385bf5f3c10
Florian Jenett <fjenett@gmail.com>
2013-04-01 20:14:29 +0200

    build requirements in place

---
+++ resources/js-header.js
@@ -0,0 +1,10 @@
+/**
+ *	A window.postMessage() middleware inspired by connect.
+ *
+ *	Mothership: https://github.com/fjenett/postmessanger-js
+ *	Made possible (in parts) by: http://motionbank.org
+ *
+ *	Started: fjenett 20130401
+ *	Version: ##version##
+ *	Build: ##build##
+ */
